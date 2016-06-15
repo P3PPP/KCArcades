09d771ebf3284f580773c97375e0f9285c6f3218
P3PPP <ticktackmob@gmail.com>
2016-06-15 21:24:17 +0900

    (一応)スマホ対応

--- KCArcades/App_Start/RouteConfig.cs
+++ KCArcades/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace KCArcades
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+				//defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+				defaults: new { controller = "ArcadesMap", action = "Index", id = UrlParameter.Optional }
 			);
 		}
 	}
---
+++ KCArcades/Controllers/ArcadesMapController.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KCArcades.Controllers
+{
+    public class ArcadesMapController : Controller
+    {
+        // GET: ArcadesMap
+        public ActionResult Index()
+        {
+			var directory = Server.MapPath("~/Content/ArcadesJson");
+			var files = Directory.GetFiles(directory);
+			var allArcades = files.Select(path =>
+			{
+				string json = null;
+				using (var reader = new StreamReader(path))
+				{
+					json = reader.ReadToEnd();
+				}
+				return json;
+			})
+			.Select(json =>
+			{
+				return JsonConvert.DeserializeObject<List<Arcade>>(json);
+			})
+			.SelectMany(x => x)
+			.ToList();
+
+			var allArcadesJson = JsonConvert.SerializeObject(allArcades);
+			ViewBag.Json = allArcadesJson;
+			return View();
+		}
+	}
+}
