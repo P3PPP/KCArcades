edd0674ca87da0bd9bf85faab14bcfd4155f4184
P3PPP <ticktackmob@gmail.com>
2016-06-20 16:27:09 +0900

    店舗情報提供システムの試作

--- KCArcades/Controllers/ArcadesMapController.cs
+++ KCArcades/Controllers/ArcadesMapController.cs
@@ -6,13 +6,22 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using KCArcades.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace KCArcades.Controllers
 {
     public class ArcadesMapController : Controller
     {
-        // GET: ArcadesMap
-        public ActionResult Index()
+		static SelectList _GPLimitationList;
+		public SelectList GPLimitationList
+		{
+			get
+			{
+				return _GPLimitationList ?? (_GPLimitationList = CreateGPLimitationList());
+			}
+		}
+		// GET: ArcadesMap
+		public ActionResult Index()
         {
 			var directory = Server.MapPath("~/Content/ArcadesJson");
 			var files = Directory.GetFiles(directory);
@@ -40,7 +49,93 @@ namespace KCArcades.Controllers
 		[HttpGet]
 		public ActionResult Report(Arcade arcade)
 		{
-			return View();
+			var report = new ArcadeReport
+			{
+				Target = arcade,
+				TargetName = arcade.Name,
+				TargetAddress = arcade.Address,
+				TargetLatitude = arcade.Latitude,
+				TargetLongitude = arcade.Longitude,
+				AdditionalInfo = "ハウスルールとか？",
+			};
+
+			ViewBag.GPLimitationList = GPLimitationList;
+
+			return View(report);
 		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken()]
+		public ActionResult Report(ArcadeReport report)
+		{
+			// 
+			//if (report.Target == null /* || 不正な値  */)
+			//{
+			//	return View("ReportFailed");
+			//}
+			if (report.TargetName == null /* || 不正な値  */)
+			{
+				return View("ReportFailed");
+			}
+
+			try
+			{
+				SendReportMail(report);
+			}
+			catch
+			{
+				return View("ReportFailed");
+			}
+
+			return View("ReportSuccessful");
+		}
+
+
+		private static SelectList CreateGPLimitationList()
+		{
+			var type = typeof(GPLimitations);
+			var items = Enum.GetValues(type)
+				.Cast<GPLimitations>()
+				.ToDictionary(p =>
+				{
+					var attribute = (DisplayAttribute)type.GetField(p.ToString())
+						.GetCustomAttributes(typeof(DisplayAttribute), false).First();
+
+					return attribute.Name;
+				}, p => (int)p);
+
+			return new SelectList(items, "Value", "Key");
+		}
+
+		private static void SendReportMail(ArcadeReport report)
+		{
+			var sender = "KanColleArcades@kcarcades.net";
+			var recipient = "kcarcades-report@gmail.com";
+			var subject = "[KCArcades]店舗情報更新リクエスト";
+
+			//店舗名：{ report.Target.Name}
+			//住所：{ report.Target.Address}
+			//座標：({ report.Target.Latitude},{ report.Target.Longitude})
+
+			var body = $@"
+# 更新対象
+店舗名：{report.TargetName}
+住所：{report.TargetAddress}
+座標：({report.TargetLatitude},{report.TargetLongitude})
+
+# 更新データ
+設置台数：{report.NumberOfMachines}
+GP制限：{report.GPLimitation}
+座標：({report.Latitude},{report.Longitude})
+追加情報：{report.AdditionalInfo}
+";
+
+			using (var client = new System.Net.Mail.SmtpClient())
+			{
+				client.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+				client.Send(sender, recipient, subject, body);
+			}
+		}
+
 	}
 }
--- KCArcades/Models/ArcadeReport.cs
+++ KCArcades/Models/ArcadeReport.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,37 @@ namespace KCArcades.Models
 {
 	public class ArcadeReport
 	{
+		public Arcade Target { get; set; }
+
+		public string TargetName { get; set; }
+		public string TargetAddress { get; set; }
+		public double TargetLatitude { get; set; }
+		public double TargetLongitude { get; set; }
+
 		public double? Latitude { get; set; }
 		public double? Longitude { get; set; }
 		public int? NumberOfMachines { get; set; }
+		public GPLimitations GPLimitation { get; set; }
 		public string AdditionalInfo { get; set; }
 	}
+
+	public enum GPLimitations
+	{
+		//[Display(Name = "")]
+		//None = 0,
+		[Display(Name = "300GP")]
+		GP300 = 1,
+		[Display(Name = "600GP")]
+		GP600,
+		[Display(Name = "900GP")]
+		GP900,
+		[Display(Name = "1200GP以上")]
+		GP1200Over,
+		[Display(Name = "無制限")]
+		Infinite = int.MaxValue,
+	}
 }
+
+
+
+
